52f25519f0118ad800a38f2552a17d000b6963da
Andreev Alexander <andreev.work.mail@gmail.com>
2021-05-23 22:51:30 +0300

    Добавлены страницы маркеты и программы

---
+++ blocks/headers/standardHeader/firstPage.js
@@ -0,0 +1,29 @@
+document.write(`<header id="header" class="site-header">
+                            <div class="wrapper">
+                                <div class="header-content d-flex justify-content-between">
+                                    <div class="header-logo">
+                                        <a class="logo logo-primary transform-scale-h" title="Logo" href="index.html">
+                                            <img src="assets/img/logo/logo.png" alt="Logo">
+                                        </a>
+                                    </div>
+
+                                    <div class="header-info info-items align-self-center">
+                                        <div class="d-flex">
+                                            <div class="info-item">
+                                                <a class="d-flex btn btn-link regular border-0 p-0 min-w-auto icon-active" href="tel:+43253312523">
+                                                    <i class="fas fas-space-r fa-phone-alt"></i>
+                                                    <span>+7 915 123-45-67</span>
+                                                </a>
+                                            </div>
+
+                                            <div class="info-item">
+                                                <a class="d-flex btn btn-link regular border-0 p-0 min-w-auto icon-active scroll-to-id" href="#contacts">
+                                                    <i class="fas fas-space-r fa-map-marker-alt"></i>
+                                                    <span>ул. Охотный ряд, д.1</span>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </header>`);
